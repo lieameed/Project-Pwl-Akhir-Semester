d81a7880b252b1b9fcb12adacccd269793b21ec0
lieameed <venansebastian@gmail.com>
2025-11-26 18:45:29 +0700

    feat: database connection

--- config/db-connections.php
+++ config/db-connections.php
@@ -5,7 +5,6 @@ $password = "";
 $database = "abc";
 
 $connection = mysqli_connect($host, $username, $password, $database);
-
 if (!$connection) {
     die('Error to connect database: ' . mysqli_connect_error());
 }
